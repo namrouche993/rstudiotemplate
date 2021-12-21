7ddae2e377ce3544f9841263c098f64fd5c57667
namrouche993 <42832704+namrouche993@users.noreply.github.com>
2021-12-21 09:58:59 +0100

    Update app.R

--- app.R
+++ app.R
@@ -16,29 +16,19 @@
 ############## JOB FOR M BEN ############## 
 ############## JOB FOR M BEN ############## 
 
-
-.libPaths(paste0(getwd(),"/R-Portable-Win/library"))
-.Library=paste0(getwd(),"/R-Portable-Win/library")
-
 library(shiny)
 library(fresh)
-#library(shinydashboard)
-#library(shinydashboardPlus)
 library(shinythemes)
 library(shinyWidgets)
 library(shinycssloaders)
-#library(ECharts2Shiny)
-#library(rAmCharts)
 library(shinyBS)
 
 library(leaflet)
-#library(htmltools)
 library(leaflet.extras)
 
 
 library(rgdal)
 library(sp)
-library(readxl)
 library(highcharter)
 library(tidyverse)
 library(excelR)
@@ -47,26 +37,15 @@ library(readxl)
 
 
 library(reactable)
-#library(grDevices)
-#library(janitor)
 
 library(shinyjs)
 library(rmapshaper)
 library(geojsonio)
 library(sass)
-#library(gt)
-#library(flextable)
 
-library(rpivotTable)
-library(shinytreeview)
 library(sparkline)
 library(RColorBrewer)
 library(sp)
-
-
- 
-options(shiny.host = '192.168.31.14')
-options(shiny.port = 8282)
 
 
 
@@ -313,49 +292,6 @@ mapdz=leaflet(algeria)%>%
 #   addProviderTiles(providers[[6]]) %>%
 #   setView(lng=1.3333,lat=30.6167,zoom=6)
 #   
-
-
-############# wilayas58
-
-
-
-algeria580 <- geojsonio::geojson_read("geoData.geojson", what = "sp")
-algeria58 <- rmapshaper::ms_simplify(algeria580, keep = 0.05, keep_shapes = TRUE)
-
-wilayas58 <- read_excel(paste0(getwd(),"/wilayas58.xlsx"))
-
-
-wilayas58$field=round(rnorm(58,1500,400))
-
-
-
-wilayas58=wilayas58 %>% arrange(c(1,28,24,50,43,39,44,12,34,36,5,49,
-                                  15,21,35,33,38,42,40,18,55,14,54,52,
-                                  57,32,23,37,19,8,17,20,7,41,58,53,11,
-                                  26,47,48,51,31,56,29,13,16,27,22,2,3,
-                                  30,10,9,4,46,6,45,25))
-
-
-
-gps=data.frame(longitude=rep(0,58),latitude=rep(0,58))
-gps=data.frame(longitude=rep(0,58),latitude=rep(0,58))
-for(i in 1:58){
-  gps[i,]=algeria58@polygons[[i]]@labpt
-}
-algeria58@data$longitude=gps$longitude
-algeria58@data$latitude=gps$latitude
-algeria58@data$wilayas=wilayas58$wilaya
-
-
-
-
-mapdz58=leaflet(algeria58)%>%
-  setView(lng = 1.63333 , lat = 28.3667, zoom = 6)%>%
-  #clearBounds()%>%
-  
-  addProviderTiles("OpenStreetMap.BZH",
-                   options = leafletOptions(minZoom = 6, maxZoom = 6,dragging = TRUE)) %>%   #or we can use addProviderTiles(one_providers)
-  setMapWidgetStyle(list(background= "#ffffff"))
 
 
 
